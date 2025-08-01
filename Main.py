e2e93386401af8bf92e4aa0cfc1b60986273bd27
Tanishq434 <Tanishqchauhan0132@gmail.com>
2025-08-02 01:58:06 +0530

    Add main.py to app folder

--- Main.py
+++ Main.py
@@ -4,8 +4,6 @@ import time
 import random
 
 app = FastAPI()
-
-# Metrics
 REQUEST_TIME = Summary('request_duration_seconds', 'Time spent on requests')
 REQUEST_COUNT = Counter('total_requests', 'Number of requests')
 
@@ -13,10 +11,10 @@ REQUEST_COUNT = Counter('total_requests', 'Number of requests')
 @REQUEST_TIME.time()
 def read_root():
     REQUEST_COUNT.inc()
-    time.sleep(random.uniform(0.2, 1.0))  # Simulate delay
+    time.sleep(random.uniform(0.2, 1.0))
     return {"message": "Hello from monitored FastAPI!"}
 
 if __name__ == "__main__":
     import uvicorn
-    start_http_server(8001)  # Prometheus metrics endpoint
+    start_http_server(8001)
     uvicorn.run(app, host="0.0.0.0", port=8000)
